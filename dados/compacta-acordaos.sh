73ce662ac9b9074486b8f0556cf54d550997abd9
leonardo3108 <leonardo3108@gmail.com>
2020-03-21 20:47:38 -0300

    Listas de acordaos selecionados para Jurisprudencia

---
+++ dados/compacta-acordaos.sh
@@ -0,0 +1,6 @@
+zip -s 95m acordaos-selecionada.zip acordaos-selecionada.csv
+zip -s 95m acordaos-selecionada-filtrados-6000.zip acordaos-selecionada-filtrados-6000.csv
+zip -s 95m acordaos-selecionada-lematizados.zip acordaos-selecionada-lematizados.csv
+zip -s 95m acordaos-unicos.zip acordaos-unicos.csv
+zip -s 95m acordaos-unicos-filtrados-6000.zip acordaos-unicos-filtrados-6000.csv
+zip -s 95m acordaos-unicos-lematizados.zip acordaos-unicos-lematizados.csv
